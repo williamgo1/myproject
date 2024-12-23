7c129411957db624d094f637416e8dc3fdb928ed
William <gouwilliam878@gmail.com>
2024-12-23 17:46:01 +0300

    added Create, Update and Delete Views

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views import View
-from django.views.generic import ListView
-from .forms import AddPostForm, UploadFileForm
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .forms import UploadFileForm, AddPostForm
 from .models import Women, Category, TagPost, UploadFiles
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -11,17 +11,6 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Войти", 'url_name': 'login'}
 ]
 
-
-# def index(request):
-#     posts = Women.published.all().select_related('cat')
-#
-#     data = {
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': 0,
-#     }
-#     return render(request, 'women/index.html', context=data)
 
 class WomenHome(ListView):
     model = Women
@@ -35,29 +24,6 @@ class WomenHome(ListView):
 
     def get_queryset(self):
         return Women.published.all().select_related('cat')
-
-
-    # template_name = 'women/index.html'
-    # extra_context = {
-    #     'title': 'Главная страница',
-    #     'menu': menu,
-    #     'posts': Women.published.all().select_related('cat'),
-    #     'cat_selected': 0,
-    # }
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['title'] = 'Главная страница'
-    #     context['menu'] = menu
-    #     context['posts'] = Women.published.all().select_related('cat')
-    #     context['cat_selected'] = int(self.request.GET.get('cat_id', 0))
-    #     return context
-
-
-# def handle_uploaded_file(f):
-#     with open(f"uploads/{f.name}", "wb+") as destination:
-#         for chunk in f.chunks():
-#             destination.write(chunk)
 
 def about(request):
     if request.method == 'POST':
@@ -82,28 +48,42 @@ def show_post(request, post_slug):
 
     return render(request, 'women/post.html', data)
 
-class AddPage(View):
-    def get(self, request):
-        form = AddPostForm()
-        data = {
+class ShowPost(DetailView):
+    # model = Women
+    template_name = 'women/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post'].title
+        context['menu'] = menu
+        return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Women.published, slug=self.kwargs[self.slug_url_kwarg])
+
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    extra_context = {
             'menu': menu,
             'title': 'Добавление статьи',
-            'form': form
-        }
-        return render(request, 'women/addpage.html', data)
+    }
 
-    def post(self, request):
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+class UpdatePage(UpdateView):
+    model = Women
+    fields = ['title', 'content', 'photo', 'is_published', 'cat']
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+    extra_context = {'menu': menu,
+                     'title': 'Редактирование статьи'
+    }
 
-        data = {
-            'menu': menu,
-            'title': 'Добавление статьи',
-            'form': form
-        }
-        return render(request, 'women/addpage.html', data)
+class DeletePage(DeleteView):
+    model = Women
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -142,16 +122,18 @@ class WomenCategory(ListView):
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
-def show_tag_postlist(request, tag_slug):
-    tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED).select_related("cat")
+class TagPostList(ListView):
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    data = {
-        'title': f"Тег: {tag.tag}",
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': None,
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag = TagPost.objects.get(slug=self.kwargs['tag_slug'])
+        context['title'] = 'Тег: ' + tag.tag
+        context['menu'] = menu
+        context['cat_selected'] = None
+        return context
 
-    return render(request, 'women/index.html', context=data)
-
+    def get_queryset(self):
+        return Women.published.filter(tags__slug=self.kwargs['tag_slug']).select_related('cat')
